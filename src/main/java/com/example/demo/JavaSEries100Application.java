8c57386cca394b808a5995f4d9d68a2479db1c46
Nik-016 <pawarnikita9022@gmail.com>
2025-08-15 16:20:56 +0530

    Day 1 - First Java API

---
+++ src/main/java/com/example/demo/JavaSEries100Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSEries100Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSEries100Application.class, args);
+	}
+
+}
