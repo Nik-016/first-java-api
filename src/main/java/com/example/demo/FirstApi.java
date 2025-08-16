43966286d013d762368ff25c719f5267e21cf445
Nik-016 <pawarnikita9022@gmail.com>
2025-08-16 17:32:02 +0530

    Day 2

--- src/main/java/com/example/demo/FirstApi.java
+++ src/main/java/com/example/demo/FirstApi.java
@@ -1,6 +1,8 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,5 +10,10 @@ public class FirstApi {
 	@GetMapping("/hello")
 	public String hello() {
 		return "Hello Recruiters!";	
+	}
+	//Day 2
+	@GetMapping("/greet/{name}")
+	public String greet(@PathVariable String name) {
+		return "Hello, " + name + " ! Welcome to my API.";
 	}
 }
