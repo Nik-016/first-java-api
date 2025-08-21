9a3ebd2ee664eeaa23f0283e7aa72df504466d72
Nik-016 <pawarnikita9022@gmail.com>
2025-08-21 16:54:53 +0530

    Day 5 - Added POST API to accept JSON input

--- src/main/java/com/example/demo/FirstApi.java
+++ src/main/java/com/example/demo/FirstApi.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,4 +30,10 @@ public class FirstApi {
 	public Person1 getPerson1() {
 		return new Person1("Nik", 22);
 	}
+	
+	@PostMapping("/addPerson")
+	public Person1 addPerson(@RequestBody Person1 person) {
+		return person;
+	}
 }
+
